8fb56294a5f912f257f26dc7256e4ab180a0721a
Hack Reactor Students <andrew@andrewzey.com>
2014-06-23 15:17:42 -0700

    Added exports to request-handler.js and required it in basic-server.js

--- server/request-handler.js
+++ server/request-handler.js
@@ -4,8 +4,7 @@
  * You'll have to figure out a way to export this function from
  * this file and include it in basic-server.js so that it actually works.
  * *Hint* Check out the node module documentation at http://nodejs.org/api/modules.html. */
-
-var handleRequest = function(request, response) {
+module.exports.handleRequest = function(request, response) {
   /* the 'request' argument comes from nodes http module. It includes info about the
   request - such as what URL the browser is requesting. */
 
@@ -16,8 +15,21 @@ var handleRequest = function(request, response) {
 
   var statusCode = 200;
 
+  /* These headers will allow Cross-Origin Resource Sharing (CORS).
+   * This CRUCIAL code allows this server to talk to websites that
+   * are on different domains. (Your chat client is running from a url
+   * like file://your/chat/client/index.html, which is considered a
+   * different domain.) */
+  var defaultCorsHeaders = {
+    "access-control-allow-origin": "*",
+    "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+    "access-control-allow-headers": "content-type, accept",
+    "access-control-max-age": 10 // Seconds.
+  };
+
   /* Without this line, this server wouldn't work. See the note
    * below about CORS. */
+
   var headers = defaultCorsHeaders;
 
   headers['Content-Type'] = "text/plain";
@@ -32,14 +44,3 @@ var handleRequest = function(request, response) {
   response.end("Hello, World!");
 };
 
-/* These headers will allow Cross-Origin Resource Sharing (CORS).
- * This CRUCIAL code allows this server to talk to websites that
- * are on different domains. (Your chat client is running from a url
- * like file://your/chat/client/index.html, which is considered a
- * different domain.) */
-var defaultCorsHeaders = {
-  "access-control-allow-origin": "*",
-  "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
-  "access-control-allow-headers": "content-type, accept",
-  "access-control-max-age": 10 // Seconds.
-};
